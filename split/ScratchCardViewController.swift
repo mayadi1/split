0fc70d6186b0f5eaecf1148baac0f0261204cd17
mayadi1 <admin@ayadios.com>
2017-07-29 20:44:07 -0700

    ScrathCard view added

--- split/ScratchCardViewController.swift
+++ split/ScratchCardViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ScratchCardViewController.swift
 //  split
 //
 //  Created by Mohamed Ayadi on 7/29/17.
@@ -9,7 +9,7 @@
 import UIKit
 import ScratchCard
 
-class ViewController: UIViewController {
+class ScratchCardViewController: UIViewController {
     var scratchCard: ScratchUIView!
 
     override func viewDidLoad() {
@@ -17,14 +17,28 @@ class ViewController: UIViewController {
         scratchCard  = ScratchUIView(frame: CGRect(x:0, y:0, width:self.view.frame.size.width, height:self.view.frame.size.height),Coupon: "nil", MaskImage: "NickelGray", ScratchWidth: CGFloat(40))
         self.view.addSubview(scratchCard)
 
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        self.dismiss(animated: false) { 
+            
+        }
+    }
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
